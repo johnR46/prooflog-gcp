186a79d30987517a94a6533268c6c42abc08e452
JohnR46 <Johnnote7@gmail.com>
2022-02-22 02:36:36 +0700

    update docker & clear handler

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ RUN pip install -r /app/requirements.txt
 
 COPY . /app
 
-CMD ["python","main.py"]
-#CMD exec gunicorn --bind :8080 --workers 1 --worker-class uvicorn.workers.UvicornWorker  --threads 8 main:app
+#CMD ["python","main.py"]
+CMD exec gunicorn --bind :8080 --workers 1 --worker-class uvicorn.workers.UvicornWorker  --threads 8 main:app
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import uvicorn
-from handler import app
+import logging
+from fastapi import FastAPI
+from fastapi.logger import logger
+from pydantic import BaseSettings
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=False, debug=False)
+from apis import proof_concept_log
+from cloud_logging.middleware import LoggingMiddleware
+from cloud_logging.setup import setup_logging
+
+
+class Settings(BaseSettings):
+    environment: str = 'development'
+
+
+settings = Settings()
+app = FastAPI()
+app.include_router(proof_concept_log.router)
+
+if settings.environment == 'production':
+    setup_logging()
+    app.add_middleware(LoggingMiddleware)
+else:
+    logger.setLevel(logging.DEBUG)
