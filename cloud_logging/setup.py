f09cca0b1ee4827ee1d0101827ecef1d768a6d97
JohnR46 <Johnnote7@gmail.com>
2022-02-26 17:52:51 +0700

    no change

--- cloud_logging/setup.py
+++ cloud_logging/setup.py
@@ -6,6 +6,7 @@ from cloud_logging.filter import GoogleCloudLogFilter
 from fastapi.logger import logger
 
 
+
 def setup_logging():
     client = google.cloud.logging.Client()
     handler = client.get_default_handler()
